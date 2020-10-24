f9ec5140d7bad17ba904338fca0937052270abc0
fengyue <fengyue.fy@bytedance.com>
2020-10-24 12:02:52 +0100

    initial

---
+++ assets/js/common.js
@@ -0,0 +1,11 @@
+/**
+ * Created by fengyue on 2020/10/24.
+ */
+$(document).ready(function() {
+    $('a.abstract').click(function() {
+        $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
+    });
+    $('a.bibtex').click(function() {
+        $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
+    });
+});
